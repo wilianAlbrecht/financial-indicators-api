c94ce06312ed2e977eb10acf8c4593788d2a5d17
wilianAlbrecht <wilian0012@hotmail.com>
2025-11-22 02:49:10 -0400

    alteração no modelo das models.

--- src/main/java/com/financial/indicators/models/StockData.java
+++ src/main/java/com/financial/indicators/models/StockData.java
@@ -2,14 +2,19 @@ package com.financial.indicators.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public abstract class StockData {
-    
+@Getter
+@Setter
+public class StockData {
+   
     private String symbol;
-    private Double currentValue;
-
+    private Double price;          // regularMarketPrice
+    private Double eps;            // earningsPerShare
+    private Double dividendTtm;    // trailingAnnualDividendRate (ou convertido do dividendYield)
 }
