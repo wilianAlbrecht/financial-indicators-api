287bddd96ef81e514044ea6a325e6cfc5c1adda3
wilianAlbrecht <wilian0012@hotmail.com>
2025-12-04 21:45:08 -0400

    configuração do cors para requisição pelo navegador.

---
+++ src/main/java/com/financial/indicators/config/CorsGlobal.java
@@ -0,0 +1,19 @@
+package com.financial.indicators.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobal implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
